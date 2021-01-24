eb732b2ff6d005146ab0cc4e2dd91e70661eaa21
Daniil Kagan <dan.kagan@yandex.by>
2021-01-24 19:58:05 +0300

    create the first WORKING thread. override run()

---
+++ src/manythread/MainAppClass.java
@@ -0,0 +1,10 @@
+package manythread;
+
+public class MainAppClass {
+    public static void main(String[] args)
+    {
+        testthread thread1 = new testthread();
+        thread1.start();
+
+    }
+}
---
+++ src/manythread/testthread.java
@@ -0,0 +1,9 @@
+package manythread;
+public class testthread extends Thread {
+public void run()
+{
+    System.out.println("Hello. I'm threading");
+}
+
+
+}
